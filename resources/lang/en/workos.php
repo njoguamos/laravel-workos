431f17bec3facfc7e04640e97d4867733806c289
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-06-09 04:56:29 +0300

    `chore: Add missing WorkOS exception messages`

---
+++ resources/lang/en/workos.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'exceptions' => [
+        'api_key_is_missing'   => 'The Workos API Key is missing. Please publish the [workos.php] configuration file and set the [api_key].',
+        'client_id_is_missing' => 'The Workos Client ID is missing. Please publish the [workos.php] configuration file and set the [client_id].',
+    ]
+];
