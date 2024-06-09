a3761b32a6e156b138f74fd38486e469303680a7
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-06-09 04:56:52 +0300

    chore: Add WorkOS exception classes

---
+++ src/Exceptions/ApiKeyIsMissing.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NjoguAmos\LaravelWorkos\Exceptions;
+
+use InvalidArgumentException;
+
+final class ApiKeyIsMissing extends InvalidArgumentException
+{
+    /**
+     * Create a new exception instance.
+     */
+    public static function create(): self
+    {
+        return new self(
+            message: trans(
+                key: 'workos::workos.exceptions.api_key_is_missing'
+            )
+        );
+    }
+}
