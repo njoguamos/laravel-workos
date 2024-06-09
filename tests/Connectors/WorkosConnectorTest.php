75896a969f06d1763480b7ca9c99dc6bf20a2e0c
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-06-09 04:57:31 +0300

    chore: Update tests

--- tests/Connectors/WorkosConnectorTest.php
+++ tests/Connectors/WorkosConnectorTest.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use NjoguAmos\LaravelWorkos\Connectors\WorkosConnector;
+use NjoguAmos\LaravelWorkos\Exceptions\ApiKeyIsMissing;
+use NjoguAmos\LaravelWorkos\Exceptions\ClientIdIsMissing;
 
 it(description: 'can bind config values', closure: function () {
     $connector = app(abstract: WorkosConnector::class);
@@ -22,4 +24,20 @@ it(description: 'set the default headers', closure: function () {
     ];
 
     expect(value: $connector->headers()->all())->toBe(expected: $expectedHeaders);
+});
+
+it(description: 'throws an exception if the api key is missing', closure: function () {
+    config()->set(key: 'workos.api_key', value: '');
+
+    $message = trans(key: 'workos::workos.exceptions.api_key_is_missing');
+
+    expect(fn () => app(abstract: WorkosConnector::class))->toThrow(exception: ApiKeyIsMissing::class, exceptionMessage: $message);
+});
+
+it(description: 'throws an exception if the client id is missing', closure: function () {
+    config()->set(key: 'workos.client_id', value: '');
+
+    $message = trans(key: 'workos::workos.exceptions.client_id_is_missing');
+
+    expect(fn () => app(abstract: WorkosConnector::class))->toThrow(exception: ClientIdIsMissing::class, exceptionMessage: $message);
 });
