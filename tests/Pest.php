f3b1a6b3fd076ef4c037c4443ca40028d9923a47
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-06-10 03:23:33 +0300

    chore: Add WorkosRequestException class for handling WorkOS request exceptions

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,5 +3,12 @@
 declare(strict_types=1);
 
 use NjoguAmos\LaravelWorkos\Tests\TestCase;
+use Saloon\Http\Faking\MockClient;
+use Saloon\MockConfig;
 
-uses(TestCase::class)->in(__DIR__);
+uses(TestCase::class)
+    ->beforeEach(fn () => MockClient::destroyGlobal())
+    ->in(__DIR__);
+
+
+MockConfig::setFixturePath('tests/Fixtures');
