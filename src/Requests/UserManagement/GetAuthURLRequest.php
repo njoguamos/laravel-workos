58c5869246e22cdf5fdea523fdbb3a7f19005643
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-06-24 18:27:15 +0300

    Update GetAuthURLRequest to include provider, redirect_uri, and response_type

--- src/Requests/UserManagement/GetAuthURLRequest.php
+++ src/Requests/UserManagement/GetAuthURLRequest.php
@@ -4,25 +4,44 @@ declare(strict_types=1);
 
 namespace NjoguAmos\LaravelWorkos\Requests\UserManagement;
 
+use JsonException;
+use NjoguAmos\LaravelWorkos\Concerns\FillableData;
+use NjoguAmos\LaravelWorkos\Contracts\Fillable;
 use NjoguAmos\LaravelWorkos\DTOs\AuthUrlDTO;
+use NjoguAmos\LaravelWorkos\Enums\Provider;
+use NjoguAmos\LaravelWorkos\Enums\ScreenHint;
 use Saloon\Enums\Method;
 use Saloon\Exceptions\Request\RequestException;
 use Saloon\Http\Request;
 use Saloon\Http\Response;
 use Saloon\RateLimitPlugin\Limit;
 
-class GetAuthURLRequest extends Request
+class GetAuthURLRequest extends Request implements Fillable
 {
+    use FillableData;
+
     protected Method $method = Method::GET;
 
-    protected array $errors = [
+    private array $errors = [
         'access_denied', 'ambiguous_connection_selector', 'connection_invalid', 'connection_strategy_invalid',
         'connection_unlinked', 'invalid_connection_selector', 'organization_invalid', 'oauth_failed',
         'server_error', 'client-id-invalid', 'redirect-uri-invalid', 'code_challenge_missing'
     ];
 
-    public function __construct(protected AuthUrlDTO $dto, protected string $client_id)
-    {
+    public function __construct(
+        protected string $client_id,
+        public readonly Provider $provider,
+        public readonly string $redirect_uri,
+        public readonly string $response_type = "code",
+        public readonly ?string $code_challenge = null,
+        public readonly ?string $code_challenge_method = null, // The only valid PKCE code challenge value is "S256"
+        public readonly ?string $connection_id = null,
+        public readonly ?string $organization_id = null,
+        public readonly ?string $state = null,
+        public readonly ?string $login_hint = null,
+        public readonly ?string $domain_hint = null,
+        public readonly ?ScreenHint $screen_hint = null,
+    ) {
     }
 
     public function resolveEndpoint(): string
@@ -32,11 +51,18 @@ class GetAuthURLRequest extends Request
 
     protected function defaultQuery(): array
     {
-        return [
-            ...$this->dto->getFilledData(),
-            'response_type' => "code",
-            'client_id'     => $this->client_id,
-        ];
+        return $this->getFilledData(['errors' ,'method']);
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function createDtoFromResponse(Response $response): mixed
+    {
+        return new AuthUrlDTO(
+            url: $response->getPsrResponse()->getHeaderLine('Location'),
+            provider: $this->provider
+        );
     }
 
     public function hasRequestFailed(Response $response): ?bool
