eb592b44d162f9fdcc6e0f427d3caf3aa3b162ea
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-06-24 18:14:33 +0300

    Update WorkosConnector to use TokenAuthenticator for default authentication

--- tests/Unit/Connectors/WorkosConnectorTest.php
+++ tests/Unit/Connectors/WorkosConnectorTest.php
@@ -1,12 +1,13 @@
 <?php
 
+/** @noinspection PhpUnhandledExceptionInspection */
 declare(strict_types=1);
 
 use NjoguAmos\LaravelWorkos\Connectors\WorkosConnector;
-use NjoguAmos\LaravelWorkos\DTOs\CodeAuthDTO;
 use NjoguAmos\LaravelWorkos\Exceptions\ApiKeyIsMissing;
 use NjoguAmos\LaravelWorkos\Exceptions\ClientIdIsMissing;
 use NjoguAmos\LaravelWorkos\Requests\UserManagement\AuthWithCodeRequest;
+use NjoguAmos\LaravelWorkos\Requests\UserManagement\GetUserRequest;
 use Saloon\Exceptions\Request\RequestException;
 use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
@@ -47,7 +48,7 @@ it(description: 'can get credentials', closure: function () {
 it(description: 'has the correct default Guzzle config', closure: function () {
     $connector = app(abstract: WorkosConnector::class);
 
-    expect($connector->config()->all())->toBe([
+    expect(value: $connector->config()->all())->toBe(expected: [
         "allow_redirects" => false
     ]);
 });
@@ -57,21 +58,47 @@ it(description: 'can get the correct exception message', closure: function (int 
         AuthWithCodeRequest::class => MockResponse::make(status: $code),
     ]);
 
-    $dto = new CodeAuthDTO(code: 'code');
-    $request = new AuthWithCodeRequest($dto);
+    $request = new AuthWithCodeRequest(code: 'code');
 
     $connector = app(abstract: WorkosConnector::class);
 
     expect(
-        value: fn () => $connector->send($request)
+        value: fn () => $connector->send(request: $request)
     )->toThrow(
         exception: RequestException::class,
         exceptionMessage: $message
     );
 })->with([
-    400 => [ 400, fn () => trans(key: 'workos::workos.errors.400') ],
-    401 => [ 401, fn () => trans(key: 'workos::workos.errors.401') ],
-    403 => [ 403, fn () => trans(key: 'workos::workos.errors.403') ],
-    404 => [ 404, fn () => trans(key: 'workos::workos.errors.404') ],
-    422 => [ 422, fn () => trans(key: 'workos::workos.errors.422') ],
+    400 => [400, fn () => trans(key: 'workos::workos.errors.400')],
+    401 => [401, fn () => trans(key: 'workos::workos.errors.401')],
+    403 => [403, fn () => trans(key: 'workos::workos.errors.403')],
+    404 => [404, fn () => trans(key: 'workos::workos.errors.404')],
+    422 => [422, fn () => trans(key: 'workos::workos.errors.422')],
 ]);
+
+
+it(description: 'should extend BaseWorkosConnector', closure: function () {
+    $connector = new WorkosConnector(
+        apiKey: 'key',
+        clientId: 'client_id',
+        apiBaseurl: 'baseurl'
+    );
+
+    expect(value: $connector)->toBeInstanceOf(class: WorkosConnector::class);
+});
+
+it(description: 'has the correct authorization header', closure: function () {
+    MockClient::global(mockData: [ GetUserRequest::class => MockResponse::make() ]);
+
+    $connector = new WorkosConnector(
+        apiKey: 'key',
+        clientId: 'client_id',
+        apiBaseurl: 'baseurl'
+    );
+
+    $response = $connector->send(request: new GetUserRequest(id: 'test'));
+
+    $authHeader = $response->getPendingRequest()->headers()->get(key: 'Authorization');
+
+    expect(value: $authHeader)->toBe(expected: 'Bearer key');
+});
